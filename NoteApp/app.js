8289baf58491ca24e42b4ea72f597aa528ac988d
MADHAV BAHL <madhavbahl10@gmail.com>
2017-08-23 10:03:55 +0530

    Parsing the arguements with yargs

--- NoteApp/app.js
+++ NoteApp/app.js
@@ -2,5 +2,25 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+const argv = yargs.argv;
+
+var command = argv._[0];
+console.log('Command : ' + command);
+// console.log('Process ARGS ' , process.argv);
+console.log('Yargs ARGS ' , argv);
+
+if(command === 'add'){
+  notes.addNote(argv.title,argv.body);
+} else if(command === 'list'){
+  notes.getAll();
+} else if(command === 'read'){
+  notes.getNote(argv.title);
+} else if(command === 'remove'){
+  notes.removeNote(argv.title);
+} else{
+  console.log('Command not recognized');
+}
--- NoteApp/notes.js
+++ NoteApp/notes.js
@@ -1,6 +1,21 @@
-console.log('Starting nodes.js');
+console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote was successful');
-  return 'New Note';
+var addNote = (title,body) => {
+  console.log('Adding note ', title , body);
+};
+var getAll = () => {
+  console.log('Getting all notes');
+};
+var getNote = (title) => {
+  console.log('Fetching data', title );
+};
+var removeNote = (title) => {
+  console.log('Removing a NOTE');
+  console.log(title , 'was removed');
+};
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
