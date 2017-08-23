d787f1ac2f1ae5e3c2d7bb1905e0ebaebeecec7d
MADHAV BAHL <madhavbahl10@gmail.com>
2017-08-23 14:39:29 +0530

    Adding and saving notes

--- NoteApp/notes.js
+++ NoteApp/notes.js
@@ -1,8 +1,31 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title,body) => {
-  console.log('Adding note ', title , body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  try{
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch(e) {
+    console.log('First NOTE :)');
+  }
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if(duplicateNotes.length === 0){
+    notes.push(note);
+    fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+  } else{
+    console.log('This note already exists');
+  }
 };
+
 var getAll = () => {
   console.log('Getting all notes');
 };
