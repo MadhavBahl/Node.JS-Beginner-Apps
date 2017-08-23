3665c0cd62100770d99d00177157f26e311b911a
MADHAV BAHL <madhavbahl10@gmail.com>
2017-08-23 12:11:44 +0530

    Setting up bootstrap for NOTES APP

---
+++ NoteApp/playground/json.js
@@ -0,0 +1,30 @@
+// var obj = {
+//   name: 'Madhav'
+// };
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log('JSON : ' , stringObj);
+// console.log('Object : ' , obj);
+
+// var personString = '{"name": "Madhav","age": 19}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some Title',
+  body: 'Some body'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync('notes.json',originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
