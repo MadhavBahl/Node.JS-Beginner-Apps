e71d3df06746e351843350fcc147c212236df1e2
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-04 19:06:58 +0530

    Working with the Backend Data Fetch Part

--- 09-automated-email/server.js
+++ 09-automated-email/server.js
@@ -13,6 +13,20 @@ app.get('/',(req,res) => {
   res.render('index.html');
 });
 
+app.post('/submit',(req,res) => {
+  var fName = req.body.firstName;
+  var lName = req.body.lastName;
+  var email = req.body.email;
+  var mobile = req.body.mobileNo;
+  var address = req.body.address;
+
+  console.log(`Name: ${fName} ${lName}`);
+  console.log(`Email: ${email}`);
+  console.log(`Contact No: ${mobile}`);
+  console.log(`Address: ${address}`);
+
+});
+
 app.listen(port,() => {
   console.log(`App is up on server ${port}`);
 })
