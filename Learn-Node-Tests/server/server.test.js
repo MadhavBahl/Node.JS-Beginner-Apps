f9d848376790c34f9247939b5cc14cae6ea3ce46
Madhav Bahl <madhavbahl10@gmail.com>
2017-12-17 15:45:31 +0530

    Add describe blocks

--- Learn-Node-Tests/server/server.test.js
+++ Learn-Node-Tests/server/server.test.js
@@ -3,25 +3,33 @@ const expect = require('expect');
 
 const app = require('./server.js').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({error: 'Page not found.'})
-        })
-        .end(done); 
-});
+describe('Server', () => {
 
-it('should contain me as a user', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Madhav Bahl',
-                age: 19
-            })
-        })
-        .end(done);
+    describe('GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({error: 'Page not found.'})
+                })
+                .end(done); 
+        });
+    });
+    
+    describe('GET /users', () => {
+        it('should contain me as a user', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Madhav Bahl',
+                        age: 19
+                    })
+                })
+                .end(done);
+        });
+    });
+    
 });
